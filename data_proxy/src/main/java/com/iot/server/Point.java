70a2e467831f894c20e93d117bfcd6596265c0c9
treyvian <davide.pozzoli6@gmail.com>
2022-06-14 21:09:05 +0200

    DEV: added gps and rssi to the point class

--- data_proxy/src/main/java/com/iot/server/Point.java
+++ data_proxy/src/main/java/com/iot/server/Point.java
@@ -3,9 +3,7 @@ package com.iot.server;
 import com.influxdb.annotations.Column;
 import com.influxdb.annotations.Measurement;
 
-import java.time.*;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
+import java.time.Instant;
 
 @Measurement(name = "point")
 public class Point {
@@ -13,6 +11,7 @@ public class Point {
     // Attributes
     @Column(tag = true)
     String id;
+    
     @Column
     Double temperature;
     @Column
@@ -21,57 +20,128 @@ public class Point {
     int gas;
     @Column
     int aqi;
+    @Column
+    int rssi;
+    @Column
+    Double latitude;
+    @Column
+    Double longitude;
 
     @Column(timestamp = true)
     private Instant time;
 
+    // Constructors
+
     public Point() {
-        this.time = Instant.now().plus(2, ChronoUnit.HOURS);
+        this.time = Instant.now();
     }
 
-    public Point(String id, Double temperature, Double humidity, int gas, int aqi) {
+    public Point(String id, 
+                 Double temperature, 
+                 Double humidity, 
+                 int gas, 
+                 int aqi, 
+                 int rssi, 
+                 Double latitude,
+                 Double longitude) {
+
         this.id = id;
         this.temperature = temperature;
         this.humidity = humidity;
         this.gas = gas;
         this.aqi = aqi;
+        this.rssi = rssi;
+        this.latitude = latitude;
+        this.longitude = longitude;
 
-        this.time = Instant.now().plus(2, ChronoUnit.HOURS);
+        this.time = Instant.now();
     }
 
-    //methods
 
-    public Double getTemperature() { return temperature; }
+    //methods
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
 
     public Double getHumidity() {
         return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
     }
 
     public int getGas() {
         return gas;
     }
 
+    public void setGas(int gas) {
+        this.gas = gas;
+    }
+
     public int getAqi() {
         return aqi;
     }
 
-    public String getId() {
-        return id;
+    public void setAqi(int aqi) {
+        this.aqi = aqi;
+    }
+
+    public int getRssi() {
+        return rssi;
+    }
+
+    public void setRssi(int rssi) {
+        this.rssi = rssi;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 
     public Instant getTime() {
         return time;
     }
 
+    public void setTime(Instant time) {
+        this.time = time;
+    }
+
     @Override
     public String toString() {
-        return "Point{" +
-                "id='" + id + '\'' +
-                ", temperature=" + temperature +
-                ", humidity=" + humidity +
-                ", gas=" + gas +
-                ", aqi=" + aqi +
-                ", time=" + time +
-                '}';
+        return "Point [aqi=" + aqi + 
+                    ", gas=" + gas + 
+                    ", humidity=" + humidity + 
+                    ", id=" + id + 
+                    ", latitude=" + latitude +   
+                    ", longitude=" + longitude + 
+                    ", rssi=" + rssi + 
+                    ", temperature=" + temperature + 
+                    ", time=" + time
+                + "]";
     }
 }
